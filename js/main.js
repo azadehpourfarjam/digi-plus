055a77542cc84024343f3ecbe581e3f6702ac467
azadeh pourfarjam <you@example.com>
2024-08-06 22:36:41 +0330

    =first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+let mobile=document.querySelector('.barBox .mobile');
+mobile.addEventListener('click',function (){
+    let boxNav=document.querySelector('.boxNav')
+    let closeNav=document.querySelector('.boxNav .close-nav')
+    boxNav.style.display='block';
+    closeNav.classList='animate close-nav';
+    closeNav.style.right='0';
+})
+let close=document.querySelector('.close-nav .close');
+close.addEventListener('click',function (){
+    let boxNav=document.querySelector('.boxNav')
+    let closeNav=document.querySelector('.boxNav .close-nav');
+    setTimeout( ()=>{
+        closeNav.classList='animate1 close-nav';
+        closeNav.style.right='-1000px';
+    })
+   setTimeout(()=>{
+       boxNav.style.display='none';
+   },1000)
+})
+
+
+
+let links=document.querySelectorAll('.nav li a');
+links=[...links];
+links.map((elem)=>{
+    elem.onmouseover=()=>{
+        elem.children[1].style.transform='scale(1,1)';
+    }
+    elem.onmouseout=()=>{
+        elem.children[1].style.transform='scale(0,0)';
+    }
+});
+
+let categoriesDeclare=document.querySelector('.categoriesBox .categoriesDeclare');
+categoriesDeclare.addEventListener('mouseover',function (){
+    categoriesDeclare.children[1].style.transform='scale(1,1)';
+})
+categoriesDeclare.addEventListener('mouseout',()=>{
+    categoriesDeclare.children[1].style.transform='scale(0,0)';
+})
